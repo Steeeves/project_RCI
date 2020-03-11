2c1c0c0daf69dda4fd1aa867390afc7ef773bfa4
PedroAntunes178 <pedronmantunes@gmail.com>
2020-03-11 23:39:49 +0000

    add

--- main.c
+++ main.c
@@ -9,23 +9,43 @@
 #include <arpa/inet.h>
 #include <netdb.h>
 #include <string.h>
+#include <stdio.h>
 
 #include "server_test.h"
 
 int max(int, int);
 
-int main(){
-  struct Server udp_server = init_udp_sv();
-  struct Server tcp_server = init_tcp_sv();
+int main(int argc, char *argv[]){
+  int ip = 0;
+  int gate = 0;
+  if(argc != 3) exit(1);
+  else{
+    sscanf(argv[1], "%d", &ip);
+    sscanf(argv[2], "%d", &gate);
+  }
+  /*printf("ip: %d\n", ip);
+  printf("gate: %d\n", gate);*/
+  struct Server udp_server = init_udp_sv(ip, gate);
+  struct Server tcp_server = init_tcp_sv(ip, gate);
   fd_set rfds;
   enum {idle, busy} state;
-  int maxfd, counter;
+  int maxfd, counter, afd = 5;
+  char teste[10];
 
   state=idle;
   while(1){
     FD_ZERO(&rfds);
     FD_SET(udp_server.fd, &rfds);
+    /*sprintf(teste, "%d", udp_server.fd);
+    printf("udp : %s\n", teste);*/
     FD_SET(tcp_server.fd, &rfds);
+    /*sprintf(teste, "%d", tcp_server.fd);
+    printf("tcp : %s\n", teste);*/
+    if(state==busy){
+      FD_SET(afd, &rfds);
+      maxfd = max(maxfd, afd) + 1;
+    }
+    FD_SET(0, &rfds);
     maxfd = max(udp_server.fd, tcp_server.fd) + 1;
 
     counter = select(maxfd, &rfds, (fd_set*)NULL, (fd_set*)NULL, (struct timeval *)NULL);
@@ -39,6 +59,11 @@ int main(){
 
     if(FD_ISSET(tcp_server.fd, &rfds)){
       tcp_server = listen_tcp_sv(tcp_server);
+    }
+
+    if(FD_ISSET(0, &rfds)){
+      scanf("%s", teste);
+      printf("Input : %s\n", teste);
     }
   }
   close_tcp_sv(tcp_server);
